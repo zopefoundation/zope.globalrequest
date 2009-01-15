93d0b663643b700ab14bd159b688ea3c584c66ce
Andreas Zeidler <az@zitc.de>
2009-01-15 22:45:36 +0000

    tag first alpha release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os.path import join
 
-version = '1.0a1'
+version = '1.0a2'
 readme = open(join('src', 'zope', 'globalrequest', 'README.txt')).read()
 history = open('CHANGES.txt').read()
 
