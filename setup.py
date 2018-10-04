b545996c983b587a0d563df09aec3c9f60642fd9
Michael Howitz <mh@gocept.com>
2018-10-04 16:08:14 +0200

    Back to development: 1.6
    [skip ci]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-version = '1.5'
+version = '1.6.dev0'
 
 readme = open('README.rst').read()
 changes = open('CHANGES.rst').read()
