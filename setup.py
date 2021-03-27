d4c9abb8ea4bfa2ef37a772b05f4d7c052406d57
Jens Vagelpohl <jens@netz.ooo>
2021-03-27 11:16:28 +0100

    - fix MANIFEST.in and setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,11 @@ setup(
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     url='https://github.com/zopefoundation/zope.globalrequest',
+    project_urls={
+        'Issue Tracker': ('https://github.com/zopefoundation/'
+                          'zope.globalrequest/issues'),
+        'Sources': 'https://github.com/zopefoundation/zope.globalrequest',
+    },
     license='ZPL',
     packages=find_packages('src'),
     package_dir={'': 'src'},
@@ -45,6 +50,7 @@ setup(
     include_package_data=True,
     platforms='Any',
     zip_safe=False,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     install_requires=[
         'setuptools',
         'zope.interface',
