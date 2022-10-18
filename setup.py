baa2d710961429000eb8b14c15d81cd5b2bcc4d1
Jens Vagelpohl <jens@plyp.com>
2022-10-18 10:57:00 +0200

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '1.6'
+version = '1.7.dev0'
 
 readme = open('README.rst').read()
 changes = open('CHANGES.rst').read()
